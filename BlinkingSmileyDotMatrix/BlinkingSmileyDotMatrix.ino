1e706e5410a586ad6fe9625c185abcae81e97e18
Jean-Luc Stevens <jeanlucstevens@ymail.com>
2023-08-01 16:55:21 +1000

    DigitShield and RGB LED Matrix test working

--- BlinkingSmileyDotMatrix/BlinkingSmileyDotMatrix.ino
+++ BlinkingSmileyDotMatrix/BlinkingSmileyDotMatrix.ino
@@ -1,11 +1,49 @@
-#include "DigitShield.h"
+#include <DigitShield.h>
+#include <Adafruit_NeoMatrix.h>
+
+#define PIN 11
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
+  NEO_MATRIX_TOP    + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(204, 0, 204), matrix.Color(204, 204, 0), matrix.Color(0, 255, 255),
+  matrix.Color(255, 10, 127), matrix.Color(127, 0, 255), matrix.Color(0, 255, 0),
+  matrix.Color(255, 99, 255)};
+
+int x = matrix.width();
+int pass = 0;
+int testDigitVal = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  DigitShield.begin()
+  DigitShield.begin();
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(colors[0]);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  DigitShield.setValue(testDigitVal);
+  matrix.fillScreen(0);    //Turn off all the LEDs
+  matrix.setCursor(x, 0);
+  matrix.print(F("GOD is KING!"));
 
+  if( --x < -110 ) {
+    x = matrix.width();
+
+    if(++pass >= 8) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  testDigitVal = testDigitVal + 1;
+  if (testDigitVal > 9999)
+  {
+    testDigitVal = 0;    
+  }
+  delay(250);  
 }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RebootCamp-ArduinoWorkshop
- 
+This is a repository of the different Arduino workshop projects for the 2023 Reboot Camp.
+This is designed to work on Arduino.
+
+## Installation
+This is how to install the required libraries.
+
+### Adafruit NeoMatrix
+1. Install via Arduino Library Manager. No other need required.
+2. Include it Arduino file - `#include <Adafruit_NeoMatrix.h>`
+
+### DigitShield
+1. Ensure the DigitShield submodule is downloaded in this repository.
+    - Can manually download via https://github.com/nootropicdesign/digit-shield is required.
+    - asdas
+2. Compress the DigitShield submodule into a .zip file.
+3. In the Arduino IDE, got to:  Menu Bar -> Sketch -> Include Library -> Add .ZIP Library.
+4. Select the Digit Shield .zip file to import DigitShield functionality.
+5. Include it Arduino file - `#include <DigitShield.h>`
+
