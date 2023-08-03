17210aae605e870df6b600dc541809a369dc5d29
Jean-Luc Stevens <jeanlucstevens@ymail.com>
2023-08-03 14:07:36 +1000

    BlinkingFace example

--- BlinkingSmileyDotMatrix/BlinkingSmileyDotMatrix.ino
+++ BlinkingSmileyDotMatrix/BlinkingSmileyDotMatrix.ino
@@ -1,49 +1,54 @@
 #include <DigitShield.h>
 #include <Adafruit_NeoMatrix.h>
 
-#define PIN 11
-
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, 11,
   NEO_MATRIX_TOP    + NEO_MATRIX_LEFT +
-  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_MATRIX_COLUMNS + NEO_TILE_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800);
 
-const uint16_t colors[] = {
-  matrix.Color(204, 0, 204), matrix.Color(204, 204, 0), matrix.Color(0, 255, 255),
-  matrix.Color(255, 10, 127), matrix.Color(127, 0, 255), matrix.Color(0, 255, 0),
-  matrix.Color(255, 99, 255)};
-
-int x = matrix.width();
-int pass = 0;
-int testDigitVal = 0;
+// Colours
+int WHITE = matrix.Color(255,255,255);
+int BLACK = matrix.Color(0,0,0);
+int RED = matrix.Color(255,0,0);
+int BLUE = matrix.Color(0,0,255);
+int GREEN = matrix.Color(0,255,0);
+int YELLOW = matrix.Color(255,255,0);
+int AQUA = matrix.Color(0,255,255);
+int PURPLE = matrix.Color(255,0,255);
 
 void setup() {
   // put your setup code here, to run once:
-  DigitShield.begin();
   matrix.begin();
-  matrix.setTextWrap(false);
   matrix.setBrightness(40);
-  matrix.setTextColor(colors[0]);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  DigitShield.setValue(testDigitVal);
-  matrix.fillScreen(0);    //Turn off all the LEDs
-  matrix.setCursor(x, 0);
-  matrix.print(F("GOD is KING!"));
 
-  if( --x < -110 ) {
-    x = matrix.width();
-
-    if(++pass >= 8) pass = 0;
-    matrix.setTextColor(colors[pass]);
-  }
+  // Normal happy face
+  matrix.fillScreen(0);          // Turn off all the LEDs
+  matrix.fillRect(1,5,2,2,RED);  // Left Eye
+  matrix.fillRect(5,5,2,2,RED);  // Right Eye
+  // Mouth
+  matrix.drawLine(2,1,5,1,RED);  // Bottom of mouth
+  matrix.drawPixel(0,3,RED);    
+  matrix.drawPixel(1,2,RED);
+  matrix.drawPixel(6,2,RED);
+  matrix.drawPixel(7,3,RED);  
   matrix.show();
-  testDigitVal = testDigitVal + 1;
-  if (testDigitVal > 9999)
-  {
-    testDigitVal = 0;    
-  }
-  delay(250);  
+  delay(800);  
+
+  // Winking happy face
+  matrix.fillScreen(0);          // Turn off all the LEDs
+  matrix.fillRect(1,5,2,2,RED);  // Left Eye
+  matrix.drawLine(5,5,6,5,RED);  // Right Winking Eye
+  // Mouth
+  matrix.drawLine(2,1,5,1,RED);  // Bottom of mouth
+  matrix.drawPixel(0,3,RED);
+  matrix.drawPixel(1,2,RED);
+  matrix.drawPixel(6,2,RED);
+  matrix.drawPixel(7,3,RED);  
+  matrix.show();
+  delay(400); 
+  
 }
